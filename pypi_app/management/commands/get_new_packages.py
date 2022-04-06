a5fec743a40370e2ad7023191178bece371b0bf9
Łukasz Kolmus <lukasz.kolmus@gmail.com>
2022-04-06 17:05:43 +0200

    add get_new_packages, modified models, migrations

--- pypi_app/management/commands/get_new_packages.py
+++ pypi_app/management/commands/get_new_packages.py
@@ -1,19 +1,56 @@
+from time import timezone
 from django.core.management.base import BaseCommand
+
+import xml.etree.ElementTree as ET
+from datetime import datetime, date
+import pytz
+
 from pypi_app.models import Item
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        
+
         new_items = []
-        
-        # TODO Import, 
-        # TODO mapping, 
-        # TODO checking if item already exists, 
-        # TODO backup db, 
-        
+
+        xml_tree = ET.parse("pypi_app/management/xml/packages.xml")
+        xml_root = xml_tree.getroot()
+
+        today = date.today()
+
+        for obj in xml_root[0].findall("item"):
+            title = obj.find("title").text[:-14]
+            link = obj.find("link").text
+            guid = obj.find("guid").text
+            description = obj.find("description").text
+            try:
+                author = obj.find("author").text
+            except AttributeError:
+                author = None
+            pub_date = datetime(
+                year=today.year,
+                month=today.month,
+                day=today.day,
+                hour=int(obj.find("pubDate").text.split(":")[0][-2:]),
+                minute=int(obj.find("pubDate").text.split(":")[1]),
+                second=int(obj.find("pubDate").text.split(":")[2][:1]),
+                microsecond=0,
+                tzinfo=pytz.UTC,
+            )
+            controll_set = Item.objects.filter(
+                title=title, link=link, guid=guid, author=author, description=description, pub_date=pub_date
+            )
+
+            if controll_set.exists():
+                continue
+            else:
+                new_items.append(
+                    Item(title=title, link=link, guid=guid, author=author, description=description, pub_date=pub_date)
+                )
+
+        # TODO backup db before save to db,
         if new_items:
             new_items_count = Item.objects.bulk_create(new_items, ignore_conflicts=True)
-            print(f'New {new_items_count} new items added to db')
+            print(f"New {len(new_items_count)} new items added to db")
         else:
             print("There is no items to add")
--- pypi_app/models.py
+++ pypi_app/models.py
@@ -5,6 +5,6 @@ class Item(models.Model):
     title = models.CharField(max_length=256)
     link = models.CharField(max_length=256)
     guid = models.CharField(max_length=256)
-    description = models.TextField(max_length=256)
-    author = models.CharField(max_length=256)
+    description = models.TextField(max_length=256, null=True)
+    author = models.CharField(max_length=256, null=True)
     pub_date = models.DateTimeField()
