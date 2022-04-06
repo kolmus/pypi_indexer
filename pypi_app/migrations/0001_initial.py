2b35f09f2fd9b6ee43db3e1bbdb028318f04bd71
Łukasz Kolmus <lukasz.kolmus@gmail.com>
2022-04-06 13:20:55 +0200

    model and integration

--- pypi_app/migrations/0001_initial.py
+++ pypi_app/migrations/0001_initial.py
@@ -7,20 +7,19 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Item',
+            name="Item",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=256)),
-                ('link', models.CharField(max_length=256)),
-                ('guid', models.CharField(max_length=256)),
-                ('description', models.TextField(max_length=256)),
-                ('author', models.CharField(max_length=256)),
-                ('pub_date', models.DateTimeField()),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("title", models.CharField(max_length=256)),
+                ("link", models.CharField(max_length=256)),
+                ("guid", models.CharField(max_length=256)),
+                ("description", models.TextField(max_length=256)),
+                ("author", models.CharField(max_length=256)),
+                ("pub_date", models.DateTimeField()),
             ],
         ),
     ]
--- pypi_app/models.py
+++ pypi_app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 
-# Create your models here.
-class Item (models.Model):
+class Item(models.Model):
     title = models.CharField(max_length=256)
     link = models.CharField(max_length=256)
     guid = models.CharField(max_length=256)
     description = models.TextField(max_length=256)
     author = models.CharField(max_length=256)
     pub_date = models.DateTimeField()
-    
