d12e4d054612687f46a39d63e4bb72a82628d890
Łukasz Kolmus <lukasz.kolmus@gmail.com>
2022-04-06 12:54:00 +0200

    model and migration

--- pypi_app/models.py
+++ pypi_app/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+class Item (models.Model):
+    title = models.CharField(max_length=256)
+    link = models.CharField(max_length=256)
+    guid = models.CharField(max_length=256)
+    description = models.TextField(max_length=256)
+    author = models.CharField(max_length=256)
+    pub_date = models.DateTimeField()
+    
